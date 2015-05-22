c08093d50e07a4fc16eefa42d4d3c9b7c7c8fea8
Hemstreet <jonhemstreet@gmail.com>
2015-05-21 21:47:03 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Plug Dj Swarm
+===
+
+"Botnet" for plugdj
+
+```
+swarm.init({
+    numBots : 1,
+    command: '$',
+    roomSlug: 'slugOfRoom',
+    masterAccount: 'NameOfYourUser',
+});
+```
---
+++ main.js
@@ -0,0 +1,11 @@
+var PlugAPI = require('plugapi');
+    swarm = require('./lib/swarm');
+
+//swarm.init({
+//    numBots : 1,
+//    command: '$',
+//    roomSlug: 'hip-hop-stacks',
+//    masterAccount: 'Klaxon',
+//});
+
+swarm.init();
